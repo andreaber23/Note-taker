a1a56c33b250a45d545b23d2945b47b2d66398a1
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-06-21 14:21:46 -0400

    deleted the route folder because it was not working properly. Instead I am coding in the server.js

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
 const express = require("express");
-const fs = require("fs");
-const app = express;
+const fs = require('fs');
+const path = require('path');
+const notesData = require("./db/db.json")
 
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
+
+// HTML route `GET /notes` should return the `notes.html` file.
+app.get("/notes", (req,res)=>{
+    res.sendFile(path.join(__dirname, "./public/notes.html "))
+});
+
+//HTML route `GET *` should return the `index.html` file.
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname, "./public/notes.html "))
+});
+
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, "./public/index.html "))
+});
+
+//`GET /api/notes` should read the `db.json` file and return all saved notes as JSON
+
+app.get('/api/notes', (req, res) => {
+      res.json(notesData);
+});
+
+
+module.exports = app;
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
