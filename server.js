f8186f141d8da16a861666124c20d2982bcb65db
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-09-09 14:32:33 -0400

    refactoring server.js paths

--- server.js
+++ server.js
@@ -1,80 +1,40 @@
-const express = require("express");
-const fs = require('fs');
+// Im port module dependencies
+const express = require('express');
 const path = require('path');
-const notesData = require("./db/db.json")
-const PORT = process.env.PORT || 3001;
+const fs = require('fs');
 const app = express();
-const { v4: uuidv4 } = require('uuid');
+const uuid = require('uuid');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// HTML route `GET /notes` should return the `notes.html` file.
-app.get("/notes", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+const notesData = path.join(__dirname, 'db', 'db.json');
+
+function readDataFromDb() {
+  const data = fs.readFileSync(notesData, 'utf-8');
+  return JSON.parse(data);
+}
+
+function writeDataToFile(data) {
+  fs.writeFileSync(notesData, JSON.stringify(data, null, 2));
+}
+
+// Route handler for the '/notes' GET request
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-//HTML route `GET *` should return the `index.html` file.
-app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/public/index.html"))
-});
-
-app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, "./public/index.html "))
-});
-
-
-
-
-// `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client
-
-app.post('/api/notes', (req, res) => {
-    const { title, text } = req.body;
-
-    if (!title || !text) {
-        return res.status(400).json({ error: 'Title and text are required.' });
-    }
-
-    const noteEntry = {
-        id: uuidv4(),
-        title,
-        text
-    };
-
-    notesData.push(noteEntry);
-    saveNotesData();
-
-    res.json(noteEntry);
-});
-
-function saveNotesData() {
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesData, null, 2)
-    );
-};
-//`GET /api/notes` should read the `db.json` file and return all saved notes as JSON
-app.get('/api/notes', (req, res) => {
-    res.json(notesData);
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    const noteId = req.params.id;
-
-    const noteIndex = notesData.findIndex(note => note.id === noteId);
-
-    if (noteIndex !== -1) {
-        notesData.splice(noteIndex, 1);
-        saveNotesData();
-
-        res.json({ message: 'Note deleted.' });
-    } else {
-        res.status(404).json({ error: 'Note could not be found.' });
-    }
+// Route handler for the root '/' GET request
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
+
+// Open PORT server 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+  console.log(`App listening at http://localhost:${PORT}`);
+});
